5a0977aa06966041a492543ffdf59418ee6ac2f0
zhangjh <njhxzhangjihong@126.com>
2025-06-16 17:16:10 +0800

    去除lovable-tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,8 +10,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
